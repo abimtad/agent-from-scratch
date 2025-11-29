9a480a5422f6392e880a56891228903d3ddae5ec
Abel Tadesse <tadesseabel26@gmail.com>
2025-11-29 22:11:14 +0300

    Add user interaction for jokes and Reddit posts, and refactor tool integration

--- index.ts
+++ index.ts
@@ -1,8 +1,6 @@
 import 'dotenv/config'
-import { runLLM } from './src/llm'
-import { addMessages, getMessages } from './src/memory'
 import { runAgent } from './src/agent'
-import { z } from 'zod'
+import { tools } from './tools'
 
 const userMessage = process.argv[2]
 
@@ -11,9 +9,4 @@ if (!userMessage) {
   process.exit(1)
 }
 
-const weatherTool = {
-  name: 'get_weather',
-  description: 'use this to get the weather',
-  parameters: z.object({}),
-}
-const response = await runAgent({ userMessage, tools: [weatherTool] })
+const response = await runAgent({ userMessage, tools })
--- src/toolRunner.ts
+++ src/toolRunner.ts
@@ -2,18 +2,32 @@ import type OpenAI from 'openai'
 import { toFloat32Array } from 'openai/core.mjs'
 import { tokenToString } from 'typescript'
 import { get_weather } from './util'
+import type { AIMessage } from '../types'
+import { generateImage } from '../tools/generateImage'
+import { dadJoke } from '../tools/dadJoke'
+import { reddit } from '../tools/reddit'
 
 export const runTool = async (
-  toolCall: OpenAI.Chat.Completions.ChatCompletionMessageToolCall
+  toolCall: OpenAI.Chat.Completions.ChatCompletionMessageToolCall,
+  userMessage: string
 ) => {
   const input = {
+    userMessage,
     toolArgs: JSON.parse(toolCall.function.arguments),
   }
 
   switch (toolCall.function.name) {
-    case 'get_weather':
-      return get_weather()
+    case 'generate_image':
+      const image = await generateImage(input)
+      return image
+
+    case 'dad_joke':
+      return dadJoke(input)
+
+    case 'reddit':
+      return reddit(input)
+
     default:
-      throw Error(`Unknown tool: ${toolCall.function.name}`)
+      throw new Error(`Unknown tool: ${toolCall.function.name}`)
   }
 }
